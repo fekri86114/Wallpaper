9e3a1f67a9907b35180b310315f950ab338d2384
Sana <sanaebadi97@gmail.com>
2022-02-16 18:02:56 +0330

    added main activity with host fragment and bottom navigation bar

--- app/src/main/java/org/rubikamp/wallpaper/fragments/HomeFragment.java
+++ app/src/main/java/org/rubikamp/wallpaper/fragments/HomeFragment.java
@@ -1,4 +1,4 @@
-package org.rubikamp.wallpaper;
+package org.rubikamp.wallpaper.fragments;
 
 import android.os.Bundle;
 
@@ -8,12 +8,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import org.rubikamp.wallpaper.R;
+
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link AboutuSFragment#newInstance} factory method to
+ * Use the {@link HomeFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class AboutuSFragment extends Fragment {
+public class HomeFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,7 +26,7 @@ public class AboutuSFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public AboutuSFragment() {
+    public HomeFragment() {
         // Required empty public constructor
     }
 
@@ -34,11 +36,11 @@ public class AboutuSFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment AboutuSFragment.
+     * @return A new instance of fragment HomeFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static AboutuSFragment newInstance(String param1, String param2) {
-        AboutuSFragment fragment = new AboutuSFragment();
+    public static HomeFragment newInstance(String param1, String param2) {
+        HomeFragment fragment = new HomeFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -59,6 +61,6 @@ public class AboutuSFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_aboutus, container, false);
+        return inflater.inflate(R.layout.fragment_home, container, false);
     }
 }
