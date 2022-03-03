b77512d2945cecb3ed7e2805404d5be9f1d3d15c
Sana Ebadi <sanaebadi97@gmail.com>
2022-03-03 20:51:17 +0330

    added layout row and added recyclerview in home freagment

--- app/src/main/java/org/rubikamp/wallpaper/fragments/HomeFragment.java
+++ app/src/main/java/org/rubikamp/wallpaper/fragments/HomeFragment.java
@@ -1,11 +1,9 @@
 package org.rubikamp.wallpaper.fragments;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -20,16 +18,7 @@ public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
 
-    @Override
-    public void onAttach(@NonNull Context context) {
-        super.onAttach(context);
-    }
 
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -41,46 +30,7 @@ public class HomeFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        binding.textviewTest.setOnClickListener(it -> Toast.makeText(getContext(), "jdhjdhcfgdg", Toast.LENGTH_SHORT).show());
     }
 
-    @Override
-    public void onViewStateRestored(@Nullable Bundle savedInstanceState) {
-        super.onViewStateRestored(savedInstanceState);
-    }
 
-    @Override
-    public void onStart() {
-        super.onStart();
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-    }
-
-    @Override
-    public void onPause() {
-        super.onPause();
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-    }
-
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-    }
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-    }
 }
